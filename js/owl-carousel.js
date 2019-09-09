4602b6b432910ed84971ff7a218184abc7779389
Michal Kokocinski <michal@waat.eu>
2019-09-09 12:35:56 +0200

    Updates

--- js/owl-carousel.js
+++ js/owl-carousel.js
@@ -1,2 +1,2 @@
 
-$(window).bind("styleguide:onRendered",function(e){$(".owl-carousel").owlCarousel({loop:true,items:1,autoPlay:true,nav:true,navText:["<i class='icon icon--arrow-left'>","<i class='icon icon--arrow-right'>"]});});
+"use strict";$(window).bind("styleguide:onRendered",function(e){$(".owl-carousel").owlCarousel({loop:true,items:1,autoPlay:true,nav:true,navText:["<i class='icon icon--arrow-left'>","<i class='icon icon--arrow-right'>"]});});
